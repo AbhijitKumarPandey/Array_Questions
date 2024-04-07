65375ba841bccea3e72a17c3745c0b999a631c3f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-04-07 13:46:34 +0530

    Q4

---
+++ EvenNumber.java
@@ -0,0 +1,17 @@
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		int arr[] = {56,87,34,43,21,22,20};
+		for(int i=1;i<arr.length; i++) {
+		//	for(int j=1; j<arr.length; j++) {
+				if(arr[i]%2==0) {
+					System.out.println("Even number is " +  arr[i]);
+				}
+		}
+	}
+}
+					
+
+	
+
+
