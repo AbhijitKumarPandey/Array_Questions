8b7c19edc003c2e027acdd9c1f17fe63738d745f
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-04-07 13:45:24 +0530

    Q2

---
+++ Search_Elements.java
@@ -0,0 +1,14 @@
+
+public class Search_Elements {
+	public static void main(String[] args) {
+		int [] arr = {6,4,3,8};
+		int x = 8;
+		int index = -1;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]==x);
+			index = i;
+		}
+		System.out.println(index);
+	}
+
+}
