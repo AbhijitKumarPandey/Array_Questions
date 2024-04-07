127f4af70539c3cede6f55ddfbd4fb501760ccb8
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-04-07 13:47:49 +0530

    Q6

---
+++ Sum_Of_All_Elements.java
@@ -0,0 +1,12 @@
+
+public class Sum_Of_All_Elements {
+	public static void main(String[] args) {
+		int arr[] = {5,6,4};
+		int sum =0;
+		for(int i=0; i<arr.length; i++) {
+			sum = arr[i];
+			}
+		System.out.println(sum);
+	}
+}
+
